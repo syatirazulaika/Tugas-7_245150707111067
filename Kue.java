322d2b0e7dea28afd6e995ff88a8b6e186bad051
syatirazulaika <syatirazulaika@gmail.com>
2025-04-27 23:37:06 +0700

    Kue.java

---
+++ Kue.java
@@ -0,0 +1,26 @@
+package PraktikumTujuh;
+
+public abstract class Kue {
+    private String nama;
+    private double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public abstract double hitungHarga();
+
+    public String toString() {
+        return "Nama Kue: " + nama + ", Harga: " + harga;
+    }
+}
+
